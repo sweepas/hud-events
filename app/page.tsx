4068d9e9e758df59a5a58d1fc08216a25eb2af61
sweepas <47362063+sweepas@users.noreply.github.com>
2024-06-19 18:50:42 +0100

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -69,9 +69,7 @@ const HomePage = ({ searchParams }: SearchParamProps) => {
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <CategoryFilter />
         </div>
-        <h2 className="h2-bold">
-          Trust by <br /> Thousands of Users in your Hood
-        </h2>
+       
 
         <div className="flex w-full flex-col gap-5 md:flex-row"></div>
 
---
+++ components/shared/Footer.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="border-t flex justify-center items-center h-16">
+      <div className="flex flex-col gap-4 p-5 text-center sm:flex-row w-full">
+        <p>The Hoob Hub. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
